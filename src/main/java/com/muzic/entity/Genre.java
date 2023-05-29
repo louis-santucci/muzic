11dffc79c0ae3412ae3e4cf6b69a6e5dfd5717f6
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-05-29 14:15:18 +0200

    [ENTITY] Updated Genre class to fix constructor error

--- src/main/java/com/muzic/entity/Genre.java
+++ src/main/java/com/muzic/entity/Genre.java
@@ -1,12 +1,16 @@
 package com.muzic.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Entity(name = "genre")
 @Table(name = "t_genre")
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class Genre extends SuperEntity {
     @Column(name = "name")
