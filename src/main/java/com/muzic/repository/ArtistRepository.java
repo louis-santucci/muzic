69aa7c8ba3512e0b7b4f7935705af31186008770
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-05-29 14:34:48 +0200

    [REPO] Added Annotation for repo

--- src/main/java/com/muzic/repository/ArtistRepository.java
+++ src/main/java/com/muzic/repository/ArtistRepository.java
@@ -2,10 +2,12 @@ package com.muzic.repository;
 
 import com.muzic.entity.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface ArtistRepository extends JpaRepository<Artist, UUID> {
     List<Artist> findArtistsByName(String name);
 }
--- src/main/java/com/muzic/repository/GenreRepository.java
+++ src/main/java/com/muzic/repository/GenreRepository.java
@@ -2,10 +2,12 @@ package com.muzic.repository;
 
 import com.muzic.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface GenreRepository extends JpaRepository<Genre, UUID> {
     List<Genre> findGenresByName(String name);
 }
