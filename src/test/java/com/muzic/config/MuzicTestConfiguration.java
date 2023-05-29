5366bd63a8c9596d300ce5f3f81e0228690a8745
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-05-29 18:02:08 +0200

    [ASYNC HANDLING] Remove Async handling

--- src/test/java/com/muzic/config/MuzicTestConfiguration.java
+++ src/test/java/com/muzic/config/MuzicTestConfiguration.java
@@ -1,11 +1,7 @@
 package com.muzic.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @TestConfiguration
-@EnableAsync
-@EnableTransactionManagement(proxyTargetClass = true)
 public class MuzicTestConfiguration {
 }
